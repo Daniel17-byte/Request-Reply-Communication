7f4d74507da474a1403977bef1df933e2f3c1c5a
daniellungu <lungud63@yahoo.com>
2024-10-24 23:05:23 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="daniellungu"
+
+ENTRYPOINT ["top", "-b"]
---
+++ fe/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.sd.fe {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.sd.fe to javafx.fxml;
+    exports org.sd.fe;
+}
