5973828850eb6c9d540f50e86ce731940f050722
daniellungu <lungud63@yahoo.com>
2024-10-27 18:40:51 +0200

    docker setup 1.2 + init react app

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Daniel is awesome!</h1>
+    </>
+  );
+}
+
+export default App;
--- client/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.sd.client {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens org.sd.client to javafx.fxml;
-    exports org.sd.client;
-}
